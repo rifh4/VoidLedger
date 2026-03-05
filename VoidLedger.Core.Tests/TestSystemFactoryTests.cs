97bbf853b453906104caa838f135de9479c7521e
rifh4 <rifhazan@gmail.com>
2026-03-05 11:28:11 +0200

    Add deterministic test system factory

---
+++ VoidLedger.Core.Tests/TestSystemFactoryTests.cs
@@ -0,0 +1,15 @@
+﻿using VoidLedger.Core.Tests.Support;
+namespace VoidLedger.Core.Tests;
+
+public sealed class TestSystemFactoryTests
+{
+    [Fact]
+    public void Create_ShouldStartWithEmptyState()
+    {
+        TestSystem system = TestSystemFactory.Create();
+        Assert.Equal(0m, system.Balance);
+        Assert.Equal(0, system.ActionCount);
+        Assert.Equal(0, system.GetHoldingQty("WATER"));
+        Assert.Equal(0m, system.GetPrice("WATER"));
+    }
+}
